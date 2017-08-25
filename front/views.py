0b5caf85a53658e0ea542853216cae6865d02288
Muhammad Hidayatulloh S <m.hidayatulloh1@gmail.com>
2017-08-25 17:39:06 +0700

    company detail

--- front/views.py
+++ front/views.py
@@ -29,6 +29,69 @@ class CompanyDetailView(TemplateView):
 	def get_context_data(self, **kwargs):
 		context = super(CompanyDetailView, self).get_context_data(**kwargs)
 
+		file_csv = os.path.join(settings.BASE_DIR, 'static', 'companies.csv')
+		read_file = open(file_csv, encoding="ISO-8859-1")
+		reader = list(csv.DictReader(read_file))
+
+		slug = self.kwargs.get('slug')
+
+		data_regulator_capital = []
+		data_npat_alloc_eq = []
+		data_total_limit_eop = []
+		data_revenue = []
+
+		total_data = 0
+ 
+		if 'and' in slug:
+			slug = slug.replace('and', '&').replace('-', ' ')
+		else:
+			slug = slug.replace('-', ' ')
+
+		for i in reader:
+			if i['CMGUnmaskedName'].lower() == slug:
+				data_regulator_capital.append({
+					'tahun': '2014',
+					'value': i[' RegulatoryCapital_AVG_FY14'].replace(',', '').strip()
+				})
+				data_regulator_capital.append({
+					'tahun': '2015',
+					'value': i[' RegulatoryCapital_AVG_FY15'].replace(',', '').strip()
+				})
+
+				data_npat_alloc_eq.append({
+					'tahun': '2014',
+					'value': i[' NPAT_AllocEq_FY14'].replace(',', '').strip()
+				})
+				data_npat_alloc_eq.append({
+					'tahun': '2015',
+					'value': i[' NPAT_AllocEq_FY15X'].replace(',', '').strip()
+				})
+
+				data_total_limit_eop.append({
+					'tahun': '2014',
+					'value': i[' TotalLimits_EOP_FY14'].replace(',', '').strip()
+				})
+				data_total_limit_eop.append({
+					'tahun': '2015',
+					'value': i[' TotalLimits_EOP_FY15x'].replace(',', '').strip()
+				})
+
+				data_revenue.append({
+					'tahun': '2014',
+					'value': i['REVENUE FY14'].replace(',', '').strip()
+				})
+				data_revenue.append({
+					'tahun': '2015',
+					'value': i['REVENYE FY15X'].replace(',', '').strip()
+				})
+
+		context.update({
+			'data_regulator_capital': data_regulator_capital,
+			'data_npat_alloc_eq': data_npat_alloc_eq,
+			'data_total_limit_eop': data_total_limit_eop,
+			'data_revenue': data_revenue
+		})
+
 		return context
 
 
