51ebcbc663c477b92ba75b2eddda221ce43bdeea
Muhammad Hidayatulloh S <m.hidayatulloh1@gmail.com>
2017-08-25 22:02:11 +0700

    testing ajax

--- front/views.py
+++ front/views.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
 from django.conf import settings
+from braces.views import AjaxResponseMixin
+from django.http import JsonResponse
 
 import os
 import csv
 
 
-class HomeView(TemplateView):
+class HomeView(AjaxResponseMixin, TemplateView):
 	template_name = 'index.html'
 
 	def get_context_data(self, **kwargs):
 		context = super(HomeView, self).get_context_data(**kwargs)
 
+		# file_csv = os.path.join(settings.BASE_DIR, 'static', 'companies.csv')
+		# read_file = open(file_csv, encoding="ISO-8859-1")
+		# reader = list(csv.DictReader(read_file))
+
+		# context.update({
+		# 	'reader': reader
+		# })
+
+		return context
+
+	def get_ajax(self, request, *args, **kwargs):
 		file_csv = os.path.join(settings.BASE_DIR, 'static', 'companies.csv')
 		read_file = open(file_csv, encoding="ISO-8859-1")
 		reader = list(csv.DictReader(read_file))
 
-		context.update({
-			'reader': reader
-		})
+		resp = {
+			'data': reader
+		}
 
-		return context
+		return JsonResponse(resp)
 
 
 class CompanyDetailView(TemplateView):
